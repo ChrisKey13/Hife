d465550e88fbc4e0c8e9cd0044100ac40c305b88
Clement Laussucq <clement.laussucq@gmail.com>
2019-06-06 17:01:08 +0200

    WIP card activity

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ import "bootstrap";
 import "../plugins/flatpickr"
 import { slideToNext, slideToPrevious } from "../components/carousel_slider"
 import { loadDynamicBannerText } from '../components/banner';
-
+import "../components/slick"
 
 slideToNext();
 
