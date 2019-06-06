5bd629edf9e5180142eb810e28a203c0558fdb56
christophe bouquet <christophe.productowner@gmail.com>
2019-06-06 13:35:39 +0200

    viewlogin

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["n Efficient", "n Inclusive", "Happier"],
+    strings: ["Efficient", "Inclusive", "Happier"],
     typeSpeed: 100,
     loop: true
   });
