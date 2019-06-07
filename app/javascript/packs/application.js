4f3c27da627910109f5141fbc3e345c3eed0305a
Nicolas Martin <nicolas.artur.martin@gmail.com>
2019-06-07 14:40:35 +0200

    secure creation form and images on heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,15 @@
-import "bootstrap";
-import "../plugins/flatpickr"
-import { slideToNext, slideToPrevious } from "../components/carousel_slider"
+import "bootstrap";;
+import "../plugins/flatpickr";
+import { slideToNext, slideToPrevious } from "../components/carousel_slider";
 import { loadDynamicBannerText } from '../components/banner';
-
+import { disableButtonIfAllNotFilled } from '../components/meeting_creation';
 
 slideToNext();
 
 slideToPrevious();
 
 loadDynamicBannerText();
+
+if (document.querySelector(".slider")) {
+  disableButtonIfAllNotFilled();
+}
