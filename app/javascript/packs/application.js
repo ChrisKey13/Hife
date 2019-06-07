ba50cebbb551bec8e687a56aebd5f3188bff4d08
christophe bouquet <christophe.productowner@gmail.com>
2019-06-07 15:22:14 +0200

    newbranch

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,8 @@ import "bootstrap";
 import "../plugins/flatpickr"
 import { slideToNext, slideToPrevious } from "../components/carousel_slider"
 import { loadDynamicBannerText } from '../components/banner';
-
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+initUpdateNavbarOnScroll();
 
 slideToNext();
 
