3429225eaab7a0cbfb0c0875edc2b26dfc78dded
Nicolas Martin <nicolas.artur.martin@gmail.com>
2019-06-11 14:23:04 +0200

    fixing some bugs to avoid js errors in console

--- app/javascript/components/meeting_slider.js
+++ app/javascript/components/meeting_slider.js
@@ -1,20 +1,13 @@
 
-let slider
-let output
-
 const addOutputToSlider = (index) => {
-  slider = document.getElementById(`text-input-meeting-slider-${index}`);
-  output = document.getElementById(`agenda-duration id-${index}`);
+  const slider = document.getElementById(`text-input-meeting-slider-${index}`);
+  const output = document.getElementById(`agenda-duration id-${index}`);
+  if (slider) {
   output.innerHTML = `${slider.value} min`; // Display the default slider value
-
-  sliderValue();
-};
-
-// Update the current slider value (each time you drag the slider handle)
-const sliderValue = () => {
   slider.addEventListener("input", (event) => {
     output.innerHTML = `${slider.value} min`;
   });
+  }
 };
 
 export { addOutputToSlider };
